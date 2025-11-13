7de5868c57c58dc203d29b3cfa3950ec9cd1184d
Hjeen <hienleeone@gmail.com>
2025-11-13 22:25:49 +0700

    Update combat.js

--- assets/js/combat.js
+++ assets/js/combat.js
@@ -9,7 +9,7 @@ const hpValidation = () => {
         player.stats.hp = 0;
         playerDead = true;
         player.deaths++;
-        addCombatLog(`You died!`);
+        addCombatLog(`Bạn đã chết!`);
         document.querySelector("#battleButton").addEventListener("click", function () {
             sfxConfirm.play();
             playerDead = false;
@@ -33,9 +33,9 @@ const hpValidation = () => {
         player.kills++;
         dungeon.statistics.kills++;
         addCombatLog(`${enemy.name} died! (${new Date(combatSeconds * 1000).toISOString().substring(14, 19)})`);
-        addCombatLog(`You earned ${nFormatter(enemy.rewards.exp)} exp.`)
+        addCombatLog(`Bạn đã kiếm được ${nFormatter(enemy.rewards.exp)} exp.`)
         playerExpGain();
-        addCombatLog(`${enemy.name} dropped <i class="fas fa-coins" style="color: #FFD700;"></i>${nFormatter(enemy.rewards.gold)} gold.`)
+        addCombatLog(`${enemy.name} rơi <i class="fas fa-coins" style="color: #FFD700;"></i>${nFormatter(enemy.rewards.gold)} vàng.`)
         player.gold += enemy.rewards.gold;
         playerLoadStats();
         if (enemy.rewards.drop) {
@@ -125,7 +125,7 @@ const playerAttack = () => {
     // Apply the calculations to combat
     enemy.stats.hp -= damage;
     player.stats.hp += lifesteal;
-    addCombatLog(`${player.name} dealt ` + nFormatter(damage) + ` ${dmgtype} to ${enemy.name}.`);
+    addCombatLog(`${player.name} gây ${nFormatter(damage)} ${dmgtype} lên ${enemy.name}.`);
     hpValidation();
     playerLoadStats();
     enemyLoadStats();
